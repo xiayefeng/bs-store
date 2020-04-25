29a6b87e4a83ba3e1f8867f338aa9882697abae0
xiayefeng <xiayu_12@yeah.net>
2020-04-26 00:15:19 +0800

    添加压缩

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import {eslint} from 'rollup-plugin-eslint';
 import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify';
+import {uglify} from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/main.js',
